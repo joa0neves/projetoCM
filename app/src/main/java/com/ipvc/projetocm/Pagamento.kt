aef0dd13132445488fbf8cf717afca6d72cb540b
Tiago Soares <tiagosoarez6@gmail.com>
2022-06-12 22:18:28 +0100

    escolha do metodo de pagamneto e detelhas do pagamento

---
+++ app/src/main/java/com/ipvc/projetocm/Pagamento.kt
@@ -0,0 +1,26 @@
+package com.ipvc.projetocm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Pagamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagamento)
+    }
+
+    fun pagarCartaoMultibanco(view: View) {
+        val intent = Intent(this,CartaoMultibanco::class.java).apply {  }
+        startActivity(intent)
+    }
+    fun pagarPayPal(view: View) {
+        val intent = Intent(this,PayPal::class.java).apply {  }
+        startActivity(intent)
+    }
+    fun pagarMbWay(view: View) {
+        val intent = Intent(this,MbWay::class.java).apply {  }
+        startActivity(intent)
+    }
+}
